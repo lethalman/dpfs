0cb4b879d0885a56429e9a74e24bfd57979391f0
Luca Bruno <luca.bruno@immobiliare.it>
2016-05-24 19:41:15 +0200

    wtf

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 "use strict";
 
+function ab2str(buf) {
+	return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
+	
+function str2ab(string) {
+	var encoder = new TextEncoder("utf-8");
+	return encoder.encode(string);
+}
+
 interface NPCrypto {
 	randomBytes(len: number);
 	keyFromString(str: string): Promise<any>;
@@ -9,13 +18,8 @@ interface NPCrypto {
 class NPWebCrypto implements NPCrypto {
 	constructor(public crypto: Crypto) {}
 	
-	static str2ab(string) {
-		var encoder = new TextEncoder("utf-8");
-		return encoder.encode(string);
-	}
-	
 	keyFromString(str) {
-		return crypto.subtle.importKey("raw", NPWebCrypto.str2ab(str), { name: "HKDF" }, false, [ "deriveKey" ]);
+		return crypto.subtle.importKey("raw", str2ab(str), { name: "HKDF" }, false, [ "deriveKey" ]);
 	}
 
 	randomBytes(len) {
@@ -26,7 +30,7 @@ class NPWebCrypto implements NPCrypto {
 
 	hkdf(baseKey, salt, info) {
 		return crypto.subtle.deriveKey(
-			{ name: "HKDF", salt: salt, info: NPWebCrypto.str2ab(info), hash: "SHA-256" } as Algorithm,
+			{ name: "HKDF", salt: salt, info: str2ab(info), hash: "SHA-256" } as Algorithm,
 			baseKey,
 			{ name: "AES-GCM", length: 256 } as Algorithm,
 			false,
@@ -44,6 +48,8 @@ type GenerateKey = () => Promise<CryptoKey>;
 
 interface NPEncKey {
 	toJSON(): any;
+	encrypt(data): Promise<any>;
+	decrypt(data, iv, mac): Promise<any>;
 }
 
 interface NPEnc {
@@ -51,9 +57,30 @@ interface NPEnc {
 }
 
 class NPEncV1Key implements NPEncKey {
-	constructor(public key: CryptoKey, public salt) {
+	constructor(public crypto: NPCrypto, public key: CryptoKey, public salt) {
 	}
 
+	async encrypt(data) {
+		var iv = this.crypto.randomBytes(12);
+		var res = await crypto.subtle.encrypt({ name: "AES-GCM", iv: iv, tagLength: 128 } as Algorithm,
+											  this.key, str2ab(data));
+		/* console.log(ab2str(res.slice(data.byteLength, 128/8))); */
+		console.log(new Uint8Array(res), res.byteLength);
+		return { data: res.slice(0, 6), mac: res.slice(data.byteLength, 128/8), iv: iv };
+	}
+
+	async decrypt(data, iv, mac) {
+		var res = await crypto.subtle.encrypt({ name: "AES-GCM", iv: iv, tagLength: 128 } as Algorithm,
+											  this.key, data);
+		var decmac = res.slice(data.byteLength, 128/8);
+		console.log(new Uint8Array(res), res.byteLength);
+		if (mac !== decmac) {
+			throw new Error("MAC do not match");
+		}
+		
+		return res;
+	}
+	
 	toJSON() {
 		return { type: "NPEncV1", salt: this.salt };
 	}
@@ -66,7 +93,7 @@ class NPEncV1 implements NPEnc {
 	async generateKey(): Promise<NPEncKey> {
 		var salt = this.crypto.randomBytes(64);
 		var key = await this.crypto.hkdf(this.baseKey, salt, "NPEncV1");
-		return new NPEncV1Key(key, salt);
+		return new NPEncV1Key(this.crypto, key, salt);
 	};
 }
 
@@ -77,18 +104,27 @@ class NP {
 	async createRoot(pw: string) {
 		var userKey = await this.crypto.keyFromString(pw);
 		var enc = new NPEncV1(this.crypto, userKey);
-		var folder = new NPFolder(enc);
+		var folder = new NPFolder();
 		return folder;
 	}
 
 	async test() {
+		var userKey = await this.crypto.keyFromString("asd");
+		var enc = new NPEncV1(this.crypto, userKey);
+		var key = await enc.generateKey();
+
+		var encobj = await key.encrypt("foobar");
+		var decdata = await key.decrypt(encobj.data, encobj.iv, encobj.mac);
+		/* console.log(decdata.byteLength, ab2str(decdata)); */
+		
 		var folder = await this.createRoot("foo");
-		console.log(folder.enc);
+
+		
 	}
 }
 
 class NPFolder {
-	constructor(public enc: NPEnc) {
+	constructor() {
 	}
 }
 
